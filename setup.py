2ea131fe64d7903c24a21340f0d2946f9da877ab
romain janvier <romain.janvier@hotmail.fr>
2023-12-05 18:00:43 +0100

    More windows flag (in order to match with upstream geogram build process)

--- setup.py
+++ setup.py
@@ -5,20 +5,38 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 # Add cpp flag recommended by upstream
 if sys.platform == "linux":
-    extra_compile_args = ["-frounding-math", "-fopenmp", "-O3", "-ffp-contract=off", "-DGEO_STATIC_LIBS"]  
+    extra_compile_args = [
+        "-frounding-math",
+        "-fopenmp",
+        "-O3",
+        "-ffp-contract=off",
+        "-DGEO_STATIC_LIBS",
+    ]
     extra_link_args = []
 if sys.platform == "win32":
-    extra_compile_args = ["/openmp", "/DGEO_STATIC_LIBS"]
-    extra_link_args = ["advapi32.lib", "Shell32.lib"] # Why this is needed?
-    
+    extra_compile_args = [
+        "/openmp",
+        "/DGEO_STATIC_LIBS",
+        "/DWIN32_LEAN_AND_MEAN",
+        "/DVC_EXTRALEAN",
+        "/DNOMINMAX" "/D_CRT_SECURE_NO_WARNINGS",
+        "/D_USE_MATH_DEFINES",
+    ]
+    extra_link_args = ["advapi32.lib", "Shell32.lib"]
+
 ext_modules = [
     Pybind11Extension(
         "delaunay_geogram",
         ["delaunay_geogram_py.cpp", "./geogram.psm.Delaunay/Delaunay_psm.cpp"],
         include_dirs=["./geogram.psm.Delaunay/"],
         extra_link_args=extra_link_args,
-        extra_compile_args = extra_compile_args
+        extra_compile_args=extra_compile_args,
     ),
 ]
 
-setup(name="delaunay_geogram", version="0.0.1", ext_modules=ext_modules, cmdclass={"build_ext": build_ext})
+setup(
+    name="delaunay_geogram",
+    version="0.0.1",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)
